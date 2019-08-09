451ee92f9c279c6d2ec68ca69f942c6d08f7b014
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-09 10:17:35 -0500

    Update index.js

--- components/Header/index.js
+++ components/Header/index.js
@@ -9,4 +9,32 @@
 //  </div >
 // And add it to the DOM in the .headerContainer component
 
-function Header() {}
+
+const ltHeader = document.querySelector('.header-container');
+ltHeader.appendChild(buildHeader());
+
+function buildHeader() {
+    //define elements
+const head = document.createElement('div');
+const date = document.createElement('span');
+const h1 = document.createElement('h1');
+const temp = document.createElement('span');
+
+//class
+
+head.classList.add('header');
+date.classList.add('date');
+temp.classList.add('temp');
+
+///content
+ date.textContent('Schmaugust 9th, 2019');
+ h1.textContent('Lambda Times');
+ temp.textContent('98 Degrees');
+
+ ///append the children
+
+ head.appendChild(date);
+ head.appendChild(h1);
+ head.appendChild(temp);
+ return head;
+}
