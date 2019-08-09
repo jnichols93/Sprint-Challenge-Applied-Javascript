5767d77cb383e2ecae5b5414616c679e7ecc60f4
jnichols93 <51006450+jnichols93@users.noreply.github.com>
2019-08-09 10:45:10 -0500

    switched order

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -8,9 +8,19 @@
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
 
+axios.get('https://lambda-times-backend.herokuapp.com/topics').then(function(response){
+    console.log(response);
+    createTabs(response.data.topics);
+})
+.catch(function(err){
+    console.log(err);
+})
 function createTabs(topicArray){
-    const topics = documnet.querySelector('.topics');
-    topicArray.array.forEach(element => {
-        
+    const topics = document.querySelector('.topics');
+    topicArray.forEach(i => {
+        const tab=document.createElement('div');
+        tab.classList.add('tab')
+        topics.appendChild(tab);
+        tab.textContent = ('topic');
     });
 }
